5944911b62feaf5d3c6451466cf98fa6ff5c466e
FernandoLepore <juliofernandolepore@gmail.com>
2022-10-26 15:54:57 -0300

    comenzando a practicar y desarrollar en el lenguaje C#

---
+++ programa1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System; 
+/*  esto es un paquete/modulo. la palabras  reservada using equivale a la 
+   palabra import en otros lenguajes */
+namespace PrimerPyeoyecto
+/* el namespace es equivalente a un agrupamiento de clases  aqui la estoy declarando */
+{
+    //ahora a definir una clase principal
+    class ClasePrincipal
+    {
+        /* declaro el metodo main principal y los metodos como buena practica llevan
+         mayuscula en la primera letra */
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hola mundo desde C#");
+        }
+    }
+}
+
+
+
